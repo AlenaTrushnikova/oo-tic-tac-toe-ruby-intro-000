9a57d9721f74e01cc34653b31c62f3607e0a7545
Alena Trushnikova <alena.yu.t@gmail.com>
2020-11-05 01:15:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,7 +86,20 @@ class TicTacToe
     end
     
     def play
-    end
+      until !over?
+    end 
+      
+      
+#       until the game is over
+#   take turns
+# end
+ 
+# if the game was won
+#   congratulate the winner
+# else if the game was a draw
+#   tell the players it ended in a draw
+# end
+    
 
 
 end
